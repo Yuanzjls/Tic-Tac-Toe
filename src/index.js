e6cad1e69923d21630ece24824e4de1e294dcf57
Yuan Zhou <yzhou@windsormachine.com>
2021-03-12 15:28:53 -0500

    Update

--- src/index.js
+++ src/index.js
@@ -20,13 +20,14 @@ class Board extends React.Component {
         super(props);
         this.state = {
             squares: Array(9).fill(null),
+            xIsnext: true,
         };
     }
 
         handleClick(i){
             const squares = this.state.squares.slice();
-            squares[i] = 'x';
-            this.setState({squares: squares});
+            squares[i] = this.state.xIsnext ? 'X':"O";
+            this.setState({squares: squares, xIsnext:!this.state.xIsnext});
         }
 
 
@@ -37,7 +38,7 @@ class Board extends React.Component {
         />;
     }
     render() {
-        const status = 'Next player: X';
+        const status = 'Next player: '+ (this.state.xIsnext? "X":"O");
 
         return (
             <div>
